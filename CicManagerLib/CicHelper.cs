e36a9213bf5f3981951b7e79c1596aa1a08f92e9
yarongot <yarong@ayecka.com>
2018-08-30 21:46:46 +0300

    CicManager

---
+++ CicManagerLib/CicHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CicManagerLib
+{
+    public static class CicHelper
+    {
+        private static List<DownConverterHandler> _downConverters = new List<DownConverterHandler>();
+        private static List<CicDecoderHandler> _cicDecoders = new List<CicDecoderHandler>();
+        private static List<CicManager> _activeConfigurations = new List<CicManager>();
+
+        public static void AddDownConverter(long deviceId, string deviceIp)
+        {
+            if (_downConverters.Any(x => x.DeviceId == deviceId)) return; // Already exists
+            _downConverters.Add(new DownConverterHandler { DeviceId = deviceId, DeviceIp = deviceIp });
+        }
+
+        public static void AddCicDecoder(long deviceId, string deviceIp)
+        {
+            if (_cicDecoders.Any(x => x.DeviceId == deviceId)) return; // Already exists
+            _cicDecoders.Add(new CicDecoderHandler { DeviceId = deviceId, DeviceIp = deviceIp });
+        }
+
+        public static bool AddConfiguration(CarrierInformation carrier, long downCoverterDeviceId, long cicDecoderDeviceId)
+        {
+            if (_activeConfigurations.Any(x => x.CarrierInformation.CarrierId == carrier.CarrierId)) return false;
+
+            var downConverter = _downConverters.FirstOrDefault(x => x.DeviceId == downCoverterDeviceId);
+            if (downConverter == null) return false;
+
+            var cicDecoder = _cicDecoders.FirstOrDefault(x => x.DeviceId == cicDecoderDeviceId);
+            if (cicDecoder == null) return false;
+
+            var manager = new CicManager { CarrierInformation = carrier, DownConverter = downConverter, Decoder = cicDecoder };
+            manager.StartConfiguration();
+            _activeConfigurations.Add(manager);
+            return true;
+        }
+
+        public static bool RemoveConfiguration(int carrierId)
+        {
+            var configuration = _activeConfigurations.FirstOrDefault(x => x.CarrierInformation.CarrierId == carrierId);
+            if (configuration == null) return false;
+            _activeConfigurations.Remove(configuration);
+            configuration.Dispose();
+            return true;
+        }
+    }
+
+    public class CarrierInformation
+    {
+        public int CarrierId { get; set; }
+        public uint CenterFrequency { get; set; }
+        public uint SymbolRate { get; set; }
+        public double Snr { get; set; }
+    }
+}
---
+++ CicManagerLib/DownConverterHandler.cs
@@ -0,0 +1,93 @@
+﻿using SnmpSharpNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CicManagerLib
+{
+    class DownConverterHandler : NetworkDevice, IDisposable
+    {
+        public event EventHandler<ConfigurationUpdateEventArgs> OnDownConverterConfigurationUpdateReceived = delegate { };
+
+        public BackgroundWorker _worker;
+
+        public DownConverterHandler()
+        {
+            // Start status polling
+            _worker = new BackgroundWorker { WorkerSupportsCancellation = true };
+            _worker.DoWork += (sender, args) =>
+            {
+                var worker = sender as BackgroundWorker;
+                var lastKeepAliveCheck = DateTime.Now;
+                var lastKeepAlive = DateTime.Now;
+                var status = new DownConverterStatusEventArgs();
+
+                while (!worker.CancellationPending)
+                {
+                    Thread.Sleep(100);
+
+                    var now = DateTime.Now;
+
+                    if ((now - lastKeepAliveCheck).TotalSeconds > 5)
+                    {
+                        using (var target = new UdpTarget(IPAddress.Parse(DeviceIp)))
+                        {
+                            try
+                            {
+                                var pdu = Pdu.GetPdu(new VbCollection(new[] { new Vb(new Oid("1.3.6.1.2.1.1.1.0")) }));
+                                var param = new AgentParameters(SnmpVersion.Ver2, new OctetString("public"));
+                                var result = target.Request(pdu, param) as SnmpV2Packet;
+                                if (result != null && result.Pdu.ErrorStatus == 0)
+                                {
+                                    lastKeepAlive = DateTime.Now;
+                                    if (!status.IsAlive)
+                                    {
+                                        status.IsAlive = true;
+                                        UpdateDeviceStatus(status);
+                                    }
+                                }
+                            }
+                            catch { }
+                        }
+
+                        lastKeepAliveCheck = now;
+                    }
+
+                    if ((now - lastKeepAlive).TotalSeconds > 30)
+                    {
+                        if (status.IsAlive)
+                        {
+                            status.IsAlive = false;
+                            UpdateDeviceStatus(status);
+                        }
+                    }
+                }
+            };
+            _worker.RunWorkerAsync();
+        }
+
+        internal void ConfigureCarrier(CarrierInformation configuration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            if (_worker != null)
+            {
+                _worker.CancelAsync();
+                _worker = null;
+            }
+        }
+    }
+
+    public class DownConverterStatusEventArgs : EventArgs
+    {
+        public bool IsAlive { get; set; }
+    }
+}
